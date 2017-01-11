39f90854dc825a5dc512ec27c790ca8131cf6e86
Paul <paulghi@gmail.com>
2017-01-11 01:51:58 -0800

    most basic happy path

--- app/src/main/java/net/home/e46/sortfoto/MainActivity.java
+++ app/src/main/java/net/home/e46/sortfoto/MainActivity.java
@@ -1,5 +1,6 @@
 package net.home.e46.sortfoto;
 
+import android.media.ExifInterface;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +10,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final String SORT_FOTO = "SortFoto";
     private Button sortButton;
 
     @Override
@@ -22,7 +29,35 @@ public class MainActivity extends AppCompatActivity {
         sortButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("dir", Environment.getExternalStorageDirectory().toString());
+                String cameraImageLocation = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() + "/Camera";
+                Log.d(SORT_FOTO, "cameraImageLocation=" + cameraImageLocation);
+                File dir = new File(cameraImageLocation);
+                for (File f : dir.listFiles()) {
+                    Log.e("file", f.getName());
+                    if (!f.isDirectory()) {
+                        try {
+                            ExifInterface info = new ExifInterface(f.getAbsolutePath());
+                            if (info != null) {
+                                String dateTime = info.getAttribute(ExifInterface.TAG_DATETIME);
+                                if (dateTime != null) {
+                                    Log.i("TAG_DATETIME", dateTime);
+                                    ImageDate imageDate = parseDateTimeTag(dateTime);
+                                    File qDir = new File(cameraImageLocation + "/" + imageDate.getYear() + "-" + imageDate.getQuarter());
+                                    if (!qDir.exists()) {
+                                        qDir.mkdir();
+                                    } else if (!qDir.isDirectory()) {
+                                        // TODO: enhance later - create a dir with a different name
+                                        Log.e("mkqdir", "existing file name conflicts with " + qDir.getName());
+                                        return;
+                                    }
+                                    f.renameTo(new File(qDir.getAbsoluteFile() + "/" + f.getName()));
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
         });
     }
@@ -30,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
     }
 
     @Override
@@ -53,5 +87,73 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private ImageDate parseDateTimeTag(String s) {
+        ImageDate imageDate = null;
+        int year;
+        int month;
+
+        if (s.split(" ").length == 2)  {
+            s = s.split(" ")[0];
+            if (s.split(":").length == 3) {
+                try {
+                    year = Integer.parseInt(s.split(":")[0]);
+                    month = Integer.parseInt(s.split(":")[1]);
+                    imageDate = new ImageDate(year, month);
+                } catch (NumberFormatException e) {
+                    // log below
+                }
+            }
+        }
+
+        if (imageDate == null) {
+            Log.e("parseDateTimeTag()", "Failed to parse TAG_DATETIME");
+        }
+
+        return imageDate;
+    }
+
+    private class ImageDate {
+        private int year;
+        private int month;
+        private Quarter quarter;
+
+        public ImageDate(int year, int month) {
+            if (month>0 && month<13) {
+                this.month = month;
+                this.quarter = Quarter.parse(this.month);
+            } else {
+                throw new InvalidParameterException("month must be between 1 and 12");
+            }
+
+            this.year = year;
+        }
+
+        public int getMonth() {
+            return month;
+        }
+        public int getYear() {
+            return year;
+        }
+        public String getQuarter() {
+            return quarter.name();
+        }
+    }
+
+    enum Quarter {
+        UNDEFINED,
+        Q1,
+        Q2,
+        Q3,
+        Q4;
+
+        public static Quarter parse(int month) {
+            try {
+                return Quarter.values()[((int) Math.ceil(month/3.0))];
+            } catch (IllegalArgumentException | NullPointerException e) {
+                return UNDEFINED;
+            }
+        }
     }
 }
